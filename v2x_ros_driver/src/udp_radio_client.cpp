1c9f997c5a0052f027b16522377e6e2200cace62
paulbourelly999 <77466294+paulbourelly999@users.noreply.github.com>
2026-09-09 11:21:16 -0400

    Add DNS name resolution to allow for use of docker dns name resolution (#164)
    <!-- Thanks for the contribution, this is awesome. -->
    
    # PR Details
    ## Description
    This PR updates the **v2x-ros-driver** to be able to handle DNS name
    resolution.
    
    Before this PR, the **v2x-ros-driver** would only accept IPv4 addresses
    in dot-decimal notation. This PR adds logic to attempt DNS name
    resolution if IPv4 dot decimal notation fails. This is especially useful
    when using docker since docker provides a DNS server for containers.
    
    When 
    <!--- Describe your changes in detail -->
    
    ## Related GitHub Issue
    
    <!--- This project only accepts pull requests related to open GitHub
    issues or Jira Keys -->
    <!--- If suggesting a new feature or change, please discuss it in an
    issue first -->
    <!--- If fixing a bug, there should be an issue describing it with steps
    to reproduce -->
    <!--- Please DO NOT name partially fixed issues, instead open an issue
    specific to this fix -->
    <!--- Please link to the issue here: -->
    
    ## Related Jira Key
    [CDAS-158
    ](https://usdot-carma.atlassian.net/browse/CDAS-158)<!-- e.g. CAR-123
    -->
    
    ## Motivation and Context
    Allow for DNS name resolution for remote IP
    <!--- Why is this change required? What problem does it solve? -->
    
    ## How Has This Been Tested?
    Unit Testing 
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Defect fix (non-breaking change that fixes an issue)
    - [x] New feature (non-breaking change that adds functionality)
    - [ ] Breaking change (fix or feature that cause existing functionality
    to change)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] I have added any new packages to the sonar-scanner.properties file
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [x] I have read the
    [**CONTRIBUTING**](https://github.com/usdot-fhwa-stol/carma-platform/blob/develop/Contributing.md)
    document.
    - [x] I have added tests to cover my changes.
    - [x] All new and existing tests passed.

--- v2x_ros_driver/src/udp_radio_client.cpp
+++ v2x_ros_driver/src/udp_radio_client.cpp
@@ -64,6 +64,26 @@ bool UdpRadioClient::connect(const std::string &remote_address,
         remote_udp_ep_ = boost::asio::ip::udp::endpoint(
             boost::asio::ip::address::from_string(remote_address), remote_port);
     }
+    catch (const boost::system::system_error &e) {
+        RCLCPP_WARN_STREAM(logger_, "Was not able to read address " << remote_address << " as IPv4 address. Error : " << e.what() );
+        RCLCPP_WARN_STREAM(logger_, "Attempting DNS name resolution ... ");
+        try {
+             boost::asio::io_context io_context;
+
+            // 1. Create a resolver to handle DNS lookups
+            boost::asio::ip::udp::resolver resolver(io_context);
+            boost::asio::ip::udp::resolver::results_type endpoints = resolver.resolve(boost::asio::ip::udp::v4(), remote_address, std::to_string(remote_port));
+            remote_udp_ep_ = *endpoints.begin();
+            RCLCPP_INFO_STREAM(logger_, "Successfully resolved " << remote_address << ":" << remote_port << " to " << remote_udp_ep_.address());
+            
+        }
+        catch( const boost::system::system_error &er) {
+            RCLCPP_ERROR_STREAM(logger_, "DNS resolution failed for address " << remote_address << ":" << remote_port << er.what());
+            // Will rethrow the exception handled here.
+            throw;
+        }
+
+    }
     catch (std::exception e)
     {
         ec = boost::asio::error::invalid_argument;
